7e31cd959ad4f333b5e426cfaa97061291315fc6
Nolan Eakins <sneakin@semanticgap.com>
2016-12-18 00:17:13 -0500

    Fixed autorun.lua for the latest OC version.

--- autorun.lua
+++ autorun.lua
@@ -1,13 +1,13 @@
 local shell = require("shell")
 local component = require("component")
-
 local args = {...}
 
-local path = args[1] or shell.getWorkingDirectory()
-shell.setPath(shell.getPath() .. ":" .. path .. "bin" .. ":" .. path .. "scripts")
-package.path = package.path .. ";" .. path .. "lib/?.lua"
-package.path = package.path .. ";" .. path .. "lib/?/init.lua"
-print("Added " .. path .. " to the search paths.")
+local disk = args[1]
+local dir = "/mnt/" .. string.sub(disk.address, 1, 3) .. "/"
+print("Adding " .. dir .. " to the search paths.")
+shell.setPath(shell.getPath() .. ":" .. dir .. "bin" .. ":" .. dir .. "scripts")
+package.path = package.path .. ";" .. dir .. "lib/?.lua"
+package.path = package.path .. ";" .. dir .. "lib/?/init.lua"
 
 -- TODO let require("rob") set all this up
 local robot = false
