3fbf39fa7c79acc2c38272967f5dff6ea44065ac
Nolan Eakins <sneakin@semanticgap.com>
2017-01-15 04:36:13 -0500

    Turned off buffering and erroring if a write fails.

--- bin/tee.lua
+++ bin/tee.lua
@@ -4,6 +4,7 @@ local files = { ["/dev/stdout"] = io.stdout }
 for _, path in ipairs(args) do
   local f = io.open(path, "w")
   if f then
+    f.bufferMode = "no"
     files[path] = f
   else
     io.stderr:write("Error opening " .. path)
@@ -17,8 +18,8 @@ repeat
   if line then
     line = line .. "\n"
     for path, f in pairs(files) do
-      if not f:write(line) then
-        io.stderr:write("Error writing to " .. path)
+      if not f:write(line) and not f:flush() then
+        error("Error writing to " .. path)
       end
     end
   end
